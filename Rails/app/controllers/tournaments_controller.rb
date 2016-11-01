9b8252feee10c4b091a0354d2d2834a4e0d10ebc
Charley Lawton <charlotte.lawton@googlemail.com>
2016-11-01 14:21:06 +0000

    player routes developed

--- Rails/app/controllers/tournaments_controller.rb
+++ Rails/app/controllers/tournaments_controller.rb
@@ -4,4 +4,14 @@ class TournamentsController < ApplicationController
     tournaments = Tournament.all
     render :json => tournaments
   end
+
+  def show
+    tournament = Tournament.find(params[:id])
+    render :json => tournament.as_json
+  end
+
+  def create
+    tournament = Tournament.create({name: params[:name], location: params[:location], lat: params[:lat], lng: params[:lng]})
+    render(json: player)
+  end
 end
